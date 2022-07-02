1008de502fe9bba11b59f91c9d5bd308df3ba8e3
Abel Mitiku <abelmgetnet@gmail.com>
2022-07-02 21:02:09 +0300

    fixed pull request travis error

--- scripts/data_preProcessing.py
+++ scripts/data_preProcessing.py
@@ -7,7 +7,7 @@ sys.path.insert(0, '../logs/')
 sys.path.append(os.path.abspath(os.path.join('..')))
 from log import App_Logger
 
-app_logger = App_Logger("../logs/data_preProcessing.log").get_app_logger()
+app_logger = App_Logger("logs/data_preProcessing.log").get_app_logger()
 
 
 class data_preProcessing_script:
@@ -15,7 +15,7 @@ class data_preProcessing_script:
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
         self.logger = App_Logger(
-            "../logs/data_preProcessing.log").get_app_logger()
+            "logs/data_preProcessing.log").get_app_logger()
 
     def drop_duplicates(self) -> pd.DataFrame:
         droped = self.df[self.df.duplicated()].index
